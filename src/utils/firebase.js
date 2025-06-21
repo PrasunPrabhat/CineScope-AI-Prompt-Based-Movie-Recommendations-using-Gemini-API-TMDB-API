e284daaa60e549a5226119a093bba916f2195ca5
PrasunPrabhat <prasun2000kir@gmail.com>
2025-06-21 15:21:58 +0530

    Securing

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { FIREBASE_API } from "./Constants/Constant";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBwRS6_zkcdd1diP1OmjqCb1Mzkxi2tPIA",
+  apiKey: FIREBASE_API,
   authDomain: "netflix-frontend-d318e.firebaseapp.com",
   projectId: "netflix-frontend-d318e",
   storageBucket: "netflix-frontend-d318e.firebasestorage.app",
