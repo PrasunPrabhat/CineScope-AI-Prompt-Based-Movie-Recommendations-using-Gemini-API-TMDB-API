3d547a860bc8c226697f0856846fb018ed2cf149
PrasunPrabhat <prasun2000kir@gmail.com>
2025-06-24 07:10:44 +0530

    Issue solved

---
+++ src/utils/Constants/Constant.js
@@ -0,0 +1,30 @@
+export const LOGO =
+  "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const PHOTO_URL =
+  "https://www.shareicon.net/data/512x512/2016/05/24/770137_man_512x512.png";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMzRmMmQ3MTIxYWExMTZiN2VlNzg4OWE4Mzg3NjRlMyIsIm5iZiI6MTc1MDI1NDg4NC4wMDQsInN1YiI6IjY4NTJjNTIzNzdiOWQ5M2VjMTU4MzQ5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.me6LPZj79ePaKD-6yTlF87lmP4NocnqH_M4xuwNN-gM",
+  },
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780";
+
+export const BG_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/7968847f-3da9-44b3-8bbb-13a46579881f/web/IN-en-20250609-TRIFECTA-perspective_32b70b51-20d4-46db-8a1a-3d5428be5f0e_medium.jpg";
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "english", name: "English" },
+  { identifier: "hindi", name: "हिन्दी" },
+  { identifier: "spanish", name: "Español" },
+  { identifier: "french", name: "Français" },
+];
+
+export const GEMINI_KEY = process.env.REACT_APP_GEMINI_KEY;
+
+export const TMDB_API_KEY = "334f2d7121aa116b7ee7889a838764e3";
